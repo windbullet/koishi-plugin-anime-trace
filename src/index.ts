e4ade80630086da883efe7c62de0a77649de4430
windbullet <2536810643@qq.com>
2025-03-07 13:34:17 +0800

    api适配，支持聊天记录发送

--- src/index.ts
+++ src/index.ts
@@ -17,22 +17,21 @@ export function apply(ctx: Context, config: Config) {
   ctx.command("以图识人.动画 [img:image]", "识别图片中的动画角色")
     .option("model", "-m <model> 选择模型：1(默认), 2", {type: /^1$|^2$/, fallback: "1"})
     .action(async ({session, options}, img) => {
-      let model = options.model === "1" ? "anime_model_lovelive" : "anime_model"
-      return h.quote(session.messageId) + await detect(model, img, session)
+      let model = options.model === "1" ? "anime_model_lovelive" : "pre_stable"
+      await detect(model, img, session)
     })
 
   ctx.command("以图识人.gal [img:image]", "识别图片中的galgame角色")
     .action(async ({session}, img) => {
-      return h.quote(session.messageId) + await detect("game_model_kirakira", img, session)
+      await detect("full_game_model_kira", img, session)
     })
 
   async function detect(model: string, img: JSX.ResourceElement, session: Session<never, never, Context>) {
     let codes = {
       17701: "图片大小过大",
       17702: "服务器繁忙，请重试",
-      17799: "不明错误发生",
       17703: "请求参数不正确",
-      17704: "API维护中",
+      17704: "API 维护中",
       17705: "图片格式不支持",
       17706: "识别无法完成（内部错误，请重试）",
       17707: "内部错误",
@@ -40,8 +39,25 @@ export function apply(ctx: Context, config: Config) {
       17709: "无法加载统计数量",
       17710: "图片验证码错误",
       17711: "无法完成识别前准备工作（请重试）",
-      17712: "需要图片名称"
-    }
+      17712: "需要图片名称",
+      17721: "服务器正常运行中",
+      17722: "图片下载失败",
+      17723: "未指定 Content-Length",
+      17724: "不是图片文件或未指定",
+      17725: "未指定图片",
+      17726: "JSON 不接受包含文件",
+      17727: "Base64 格式错误",
+      17728: "已达到本次使用上限",
+      17729: "未找到选择的模型",
+      17730: "检测 AI 图片失败",
+      17731: "服务利用人数过多，请重新尝试",
+      404: "页面不存在",
+      17732: "已过期",
+      17733: "反馈成功",
+      17734: "反馈失败",
+      17735: "反馈识别效果成功",
+      17736: "验证码错误"
+  }
 
     let url: string
     if (img) {
@@ -54,12 +70,12 @@ export function apply(ctx: Context, config: Config) {
         if (msg !== undefined) {
           element = h.parse(msg)
         } else {
-          return '超时'
+          await session.send('超时')
         }
       }
 
       const image = h.select(element, 'img')
-      if (image.length === 0) return '这看上去不是图片'
+      if (image.length === 0) await session.send('这看上去不是图片')
 
       url = image[0].attrs.src
     }
@@ -70,17 +86,17 @@ export function apply(ctx: Context, config: Config) {
     let formData = new FormData()
     formData.append("model", model)
     formData.append("ai_detect", "1")
-    formData.append("image", new Blob([imageBuffer]), "image.png")
+    formData.append("file", new Blob([imageBuffer]), "image.png")
 
-    let res = JSON.parse(await ctx.http.post("https://aiapiv2.animedb.cn/ai/api/detect", formData))
-    if (![200, 17720, 0].includes(res.code)) return codes[res.code]
-    if (res.data.length === 0 ) return "未识别到角色"
-    let result = `${res.ai ? "可能是AI图\n" : ""}`
-    for (let charactor of res.data) {
+    let res = await ctx.http.post("https://api.animetrace.com/v1/search", formData)
+    if (![200, 17720, 0].includes(res.code)) await session.send(codes[res.code])
+    if (res.data.length === 0 ) await session.send("未识别到角色")
+
+    for (let character of res.data) {
       let image = await Jimp.read(imageBuffer)
       let height = image.bitmap.height
       let width = image.bitmap.width
-      let box = charactor.box
+      let box = character.box
       image.crop({
         x: box[0] * width, 
         y: box[1] * height, 
@@ -88,11 +104,26 @@ export function apply(ctx: Context, config: Config) {
         h: (box[3] - box[1]) * height
       })
       let buffer = await image.getBuffer("image/png")
-      result += `${h.image(buffer, "image/png")}
-角色：${charactor.name}
-来自：${charactor.cartoonname}\n`
+
+      let result = `<message forward>
+<message>
+<author id="${session.selfId}"/>
+${h.image(buffer, "image/png")}
+${session.username}识别的该角色可能是：
+</message>`
+
+      for (let possibleCharacter of character.character) {
+        result += `<message>
+<author id="${session.selfId}"/>
+角色名：${possibleCharacter.character}
+来自：${possibleCharacter.work}
+</message>`
+      }
+
+      result += `</message>`
+
+      await session.send(result)
     }
-    return result
   }
 
 }
